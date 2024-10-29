9f54df16043155fb1c437bd4b9fb4e31971e1a23
Ou <orongxing@gmail.com>
2024-10-29 14:33:28 +0800

    chore: use hash route

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client"
-import { RouterProvider, createRouter } from "@tanstack/react-router"
+import { RouterProvider, createHashHistory, createRouter } from "@tanstack/react-router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { routeTree } from "./routeTree.gen"
 
 const queryClient = new QueryClient()
 
+const hashHistory = createHashHistory()
+
 const router = createRouter({
+  history: hashHistory,
   routeTree,
   context: {
     queryClient,
